4b7eddf5d0eff95e4ea3c56d772ac42b1173153c
Maria Quadriq <mquadri23@gmail.com>
2021-04-15 00:03:14 -0500

    added favicon

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>myMenu Login</h1>
+    <h1>myMenu Testing navbar</h1>
     <nav>
       {isLoggedIn ? (
         <div>
