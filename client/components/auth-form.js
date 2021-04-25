7e88aaa1f36e17e33c6ae7e4677730072d722b6b
Maria Quadriq <mquadri23@gmail.com>
2021-04-25 15:02:20 -0500

    added styling for login component

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -10,26 +10,32 @@ const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
+    <div className="container-login">
+      <div className="square" />
+      <div className="form-container">
+        <form onSubmit={handleSubmit} name={name}>
+          <div id="input-email">
+            <label htmlFor="email">
+              <small>Email</small>
+            </label>
+            <input name="email" type="text" />
+          </div>
+
+          <div id="input-password">
+            <label htmlFor="password">
+              <small>Password</small>
+            </label>
+            <input name="password" type="password" />
+          </div>
+
+          <div id="btn1">
+            <button type="submit">{displayName}</button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+        </form>
+
+        <a href="/auth/google">{displayName} with Google</a>
+      </div>
     </div>
   )
 }
--- client/routes.js
+++ client/routes.js
@@ -19,7 +19,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/main" component={Main} />
         {isLoggedIn && (
