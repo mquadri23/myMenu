f3027d152536366b00d70e42d1ed12321ee79aa0
Maria Quadriq <mquadri23@gmail.com>
2021-04-24 23:56:47 -0500

    added background color and some styling

--- client/components/Main.js
+++ client/components/Main.js
@@ -3,7 +3,12 @@ import React, {Component} from 'react'
 class Main extends Component {
   render() {
     console.log('I am in this page')
-    return <h1>Main Page</h1>
+    return (
+      <div>
+        <h1>Welome to myMenu!</h1>
+        <h2>Let's get started by selecting a calendar </h2>
+      </div>
+    )
   }
 }
 export default Main
