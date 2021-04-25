c7dcb109a4f7aec6b952c1921f882e39816e3430
Maria Quadriq <mquadri23@gmail.com>
2021-04-24 23:40:30 -0500

    added route for main

--- client/routes.js
+++ client/routes.js
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route exact path="/" component={Main} />
+        <Route exact path="/main" component={Main} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
